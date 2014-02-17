7f14b26f7aebefe88d06c49f17fb3e6150aeeff5
GeorgeErickson <george55@mit.edu>
2014-02-17 11:53:18 -0500

    Removing

--- www/js/app.js
+++
@@ -1,6 +0,0 @@
-angular.module('gee', []).controller('ContactCtrl', function($scope) {
-  return $scope.submit_hire = function() {
-    this.hf.submitted = true;
-    return console.log(this.data);
-  };
-});
